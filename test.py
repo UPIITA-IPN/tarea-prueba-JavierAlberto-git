b42de7b9ba4b58fb1597e9c0d560e686cef79b4b
JavierAlberto-git <javieralberto1728@gmail.com>
2026-03-12 08:12:49 -0600

    fix stdin input for autograder

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 import sys
 
-a = int(sys.argv[1])
-b = int(sys.argv[2])
+def leer_entrada():
+    if len(sys.argv) >= 3:
+        return int(sys.argv[1]), int(sys.argv[2])
+    
+    datos = sys.stdin.read().strip().split()
+    if len(datos) >= 2:
+        return int(datos[0]), int(datos[1])
+
+    sys.exit(1)
+
+a, b = leer_entrada()
 
 print(a + b)
